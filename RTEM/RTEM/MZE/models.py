4f36e9f95433db434700427055cecfd6f9502093
GrzegorzT <144318154+GrzegorzTwicz@users.noreply.github.com>
2024-02-29 12:44:17 +0100

    .

--- RTEM/RTEM/MZE/models.py
+++ RTEM/RTEM/MZE/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 import uuid
+from django.utils.timezone import now
 
 
-def generate_default_serial_number():
-    return uuid.uuid4().hex  # Generuje unikalny identyfikator
+def generate_serial_number():
+    # Używa aktualnego czasu i krótkiego losowego UUID dla unikalności
+    timestamp = now().strftime('%Y%m%d%H%M%S')
+    unique_id = uuid.uuid4().hex[:6]  # Pobiera 6 pierwszych znaków z UUID
+    return f"{timestamp}-{unique_id}"
 
 
 class Device(models.Model):
-    serial_number = models.CharField(max_length=100, unique=True, default=generate_default_serial_number)
+    serial_number = models.CharField(max_length=100, unique=True, default=generate_serial_number)
     type = models.CharField(max_length=100)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.type} - {self.serial_number}"
-
 
 
 class TemperatureMeasurement(models.Model):
