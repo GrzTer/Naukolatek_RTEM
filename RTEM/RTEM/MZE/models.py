ce8c7cb5f0fe79cb36be6593d46beb38af672e0c
GrzegorzT <144318154+GrzegorzTwicz@users.noreply.github.com>
2024-02-29 11:25:48 +0100

    .

--- RTEM/RTEM/MZE/models.py
+++ RTEM/RTEM/MZE/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Sensor(models.Model):
+    identifier = models.CharField(max_length=100, unique=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='sensors')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.identifier
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='devices')
+    sensors = models.ManyToManyField(Sensor, related_name='devices')
+
+    def __str__(self):
+        return self.name
+
+
+class EnergyConsumptionRecord(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='consumption_records')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    energy_consumed = models.FloatField()  # wartość w kWh
+
+    def __str__(self):
+        return f"{self.sensor.identifier} - {self.timestamp} - {self.energy_consumed}kWh"
--- RTEM/RTEM/RTEM/settings.py
+++ RTEM/RTEM/RTEM/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MZE',
+    'OZ',
+    'PA',
+    'PZ',
+    'SG'
 ]
 
 MIDDLEWARE = [
