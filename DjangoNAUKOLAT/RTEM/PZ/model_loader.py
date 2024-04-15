678478e4916285a04fb0d1ee17ad0aad10ee8705
GrzegorzT <144318154+GrzegorzTwicz@users.noreply.github.com>
2024-04-15 02:26:36 +0200

    Workin? NOT really, but somehow it does.

---
+++ DjangoNAUKOLAT/RTEM/PZ/model_loader.py
@@ -0,0 +1,6 @@
+from tf_keras.models import load_model
+
+def get_model():
+    model_path = 'PZ/model_checkpoint.h5'
+    model = load_model(model_path)
+    return model
--- DjangoNAUKOLAT/RTEM/PZ/views.py
+++ DjangoNAUKOLAT/RTEM/PZ/views.py
@@ -1,20 +1,40 @@
-from django.http import JsonResponse
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.initializers import Orthogonal
-from tensorflow.python import saved_model
-# Load the model
+from .model_loader import get_model
+from django.shortcuts import render
 
-model = saved_model.load(
-    "PZ/model_checkpoint.h5")
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+
+def preprocess(data, sequence_length=720):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data_scaled = scaler.fit_transform(data.reshape(-1, 1))  # Reshape data if it's a single series
+
+    # Create sequences
+    sequences = []
+    for i in range(len(data_scaled) - sequence_length + 1):
+        sequence = data_scaled[i:i + sequence_length]
+        sequences.append(sequence)
+
+    # Convert to numpy array and reshape to fit LSTM input (samples, time steps, features)
+    sequences = np.array(sequences)
+    return sequences, scaler
 
 
 def predict(request):
-    # Load data from CSV
-    data = pd.read_csv("PZ/data1.csv")
+    model = get_model()
+    data = pd.read_csv('path/to/data1.csv')['energy_consumption']
 
-    # Assuming your model expects the same columns and no preprocessing is needed:
-    predictions = model.predict(data)
+    # Preprocess the data
+    if len(data) < 720:
+        return render(request, 'error.html',
+                      {'error': 'Not enough data for prediction. At least 720 data points required.'})
 
-    # Return predictions as JSON
-    return JsonResponse({"predictions": predictions.tolist()})
+    processed_data, scaler = preprocess(data.values, sequence_length=720)
+
+    # Make predictions
+    predictions = model.predict(processed_data)
+    predictions = [float(pred) for pred in predictions.flatten()]
+
+    return render(request, 'PZ.html', {'predictions': predictions})
+
