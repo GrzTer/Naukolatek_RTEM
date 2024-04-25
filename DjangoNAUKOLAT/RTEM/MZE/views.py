9363f0c5383f9aeab7d54302267e76b0712b545e
GrzegorzT <144318154+GrzegorzTwicz@users.noreply.github.com>
2024-04-25 19:44:44 +0200

    css dance ended, time for break.............

--- DjangoNAUKOLAT/RTEM/MZE/views.py
+++ DjangoNAUKOLAT/RTEM/MZE/views.py
@@ -3,15 +3,18 @@ from django.core.serializers.json import DjangoJSONEncoder
 from .models import EnergyConsumption
 import json
 
+
 def chart_view(request):
-    data = EnergyConsumption.objects.order_by('timestamp').values('timestamp', 'energy_consumption', 'device_id')
+    data = EnergyConsumption.objects.order_by("timestamp").values(
+        "timestamp", "energy_consumption", "device_id"
+    )
     formatted_data = [
         {
-            'timestamp': item['timestamp'].isoformat(),
-            'energy_consumption': format(float(item['energy_consumption']), '.5f'),
-            'device_id': item['device_id']
+            "timestamp": item["timestamp"].isoformat(),
+            "energy_consumption": format(float(item["energy_consumption"]), ".5f"),
+            "device_id": item["device_id"],
         }
         for item in data
     ]
     chart_data = json.dumps(formatted_data, cls=DjangoJSONEncoder)
-    return render(request, 'MZE.html', {'chart_data': chart_data})
+    return render(request, "MZE.html", {"chart_data": chart_data})
