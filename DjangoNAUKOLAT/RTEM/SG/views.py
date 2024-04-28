84163e7899cdcdaa33e501ad9b97a3a9747f41d6
GrzegorzT <144318154+GrzegorzTwicz@users.noreply.github.com>
2024-04-28 14:45:30 +0200

    SG z wyborem okresu do wyświetlenia, nakładają się dane z powodu godzinowych danych

--- DjangoNAUKOLAT/RTEM/SG/views.py
+++ DjangoNAUKOLAT/RTEM/SG/views.py
@@ -1,58 +1,70 @@
 import pandas as pd
 from django.http import JsonResponse
 from django.views import View
-from django.core.paginator import Paginator
 from django.shortcuts import render
 from entsoe import EntsoePandasClient
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.conf import settings
-
+from django.utils.timezone import make_aware, get_default_timezone
 
 class EnergyPricesView(View):
     def post(self, request):
         country_code = request.POST.get("country_code", "PL").upper()
-        year = request.POST.get("year")
-        month = request.POST.get("month")
+        date_selected = request.POST.get('date')
+        week_selected = request.POST.get('week')
+        month_selected = request.POST.get('month')
 
-        fetch_historical_data(country_code=country_code)
+        data_dir = os.path.join(settings.BASE_DIR, 'SG/data')
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
 
-        data_dir = os.path.join(settings.BASE_DIR, 'SG')
         data_path = os.path.join(data_dir, f"energy_prices_{country_code}.csv")
+        # # Attempt to load existing data if available
+        # try:
+        #     df = pd.read_csv(data_path, parse_dates=['date'])
+        # except FileNotFoundError:
+        #     df = pd.DataFrame(columns=['date', 'price'])
+        #
+        # # Fetch data if not available
+        # if df.empty or (time_span == 'day' and not df['date'].dt.date.eq(now().date()).any()):
+        #     df = fetch_historical_data(country_code, data_path)
+        df = pd.read_csv(data_path, parse_dates=['date'])
+        df['date'] = pd.to_datetime(df['date'], utc=True).dt.tz_convert(get_default_timezone())
 
-        try:
-            df = pd.read_csv(data_path)
-            df["date"] = pd.to_datetime(df["date"], utc=True)
+        if date_selected:
+            selected_date = datetime.strptime(date_selected, '%Y-%m-%d')
+            selected_date = make_aware(selected_date)
+            start_of_day = selected_date
+            end_of_day = start_of_day + timedelta(days=1, microseconds=-1)
+            df = df[(df['date'] >= start_of_day) & (df['date'] <= end_of_day)]
+        elif week_selected:
+            year, week = map(int, week_selected.split('-W'))
+            df['year'] = df['date'].dt.isocalendar().year
+            df['week'] = df['date'].dt.isocalendar().week
+            df = df[(df['year'] == year) & (df['week'] == week)]
+        elif month_selected:
+            year, month = map(int, month_selected.split('-'))
+            df = df[(df['date'].dt.year == year) & (df['date'].dt.month == month)]
 
-            if year:
-                df = df[df["date"].dt.year == int(year)]
-            if month:
-                df = df[df["date"].dt.month == int(month)]
+        if not date_selected:
+            df = df.resample('D', on='date').agg({'price': 'mean'}).reset_index()
 
-            df.sort_values(by="date", inplace=True)
+        df.sort_values(by="date", inplace=True)
+        chart_data = {
+            "categories": df['date'].dt.strftime('%Y-%m-%d %H:%M:%S').tolist(),
+            "data": df['price'].tolist()
+        }
 
-            paginator = Paginator(df, 100)
-            page_obj = paginator.get_page(request.POST.get("page", 1))
-
-            chart_data = {
-                "categories": [date.strftime("%Y-%m-%d") for date in page_obj.object_list["date"]],
-                "data": [{"x": date.strftime("%Y-%m-%d"), "y": price} for date, price in
-                         zip(page_obj.object_list["date"], page_obj.object_list["price"])]
-            }
-
-            return JsonResponse(chart_data)
-        except FileNotFoundError:
-            return JsonResponse({'error': 'Data file not found. Please try fetching data again.'}, status=404)
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+        return JsonResponse(chart_data)
 
 def fetch_historical_data(country_code='PL', start_date=None, end_date=None):
     api_key = settings.ENTSOE_API_KEY
     client = EntsoePandasClient(api_key=api_key)
-    data_dir = os.path.join(settings.BASE_DIR, 'SG')
+    data_dir = os.path.join(settings.BASE_DIR, 'SG/data')
 
     if not start_date:
-        end_date = pd.Timestamp(datetime.now(), tz="Europe/Warsaw")
+        end_date = make_aware(datetime.now())
         start_date = end_date - pd.DateOffset(years=1)
 
     try:
@@ -74,7 +86,6 @@ def fetch_historical_data(country_code='PL', start_date=None, end_date=None):
     except Exception as e:
         print(f"An error occurred while fetching or saving data: {str(e)}")
         raise
-
 
 def show_chart(request):
     context = {}
